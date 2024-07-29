cdb7ce5f9d32e58468549500ec9ac50e777d4f9a
Aerobeam <jamiecontactform@gmail.com>
2024-07-29 05:48:13 +0100

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="Aerobeam company proposes the novel solution of utilising laser ablation for cleaning wind turbine blades. wind turbine contamination is a significant barrier for wind energy companies and nations transitioning towards a sustainable future..">
+<link rel="canonical" href="https://aerobeam.ie">
+<link rel="shortlink" href="https://aerobeam.ie">
+<meta property="og:site_name" content="Aerobeam">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://aerobeam.ie">
+<meta property="og:title" content="Home | Aerobeam">
+<meta property="og:description" content="Aerobeam company proposes the novel solution of utilising laser ablation for cleaning wind turbine blades. wind turbine contamination is a significant barrier for wind energy companies and nations transitioning towards a sustainable future..">
+<meta property="fb:app_id" content="238312316239555">
+<meta name="twitter:card" content="summary">
+<meta name="twitter:description" content="Aerobeam company proposes the novel solution of utilising laser ablation for cleaning wind turbine blades. wind turbine contamination is a significant barrier for wind energy companies and nations transitioning towards a sustainable future.">
+<meta name="twitter:site" content="@Aerobeam">
+<meta name="twitter:title" content="Home | Aerobeam">
+<meta name="Generator" content="Drupal 10 (https://www.drupal.org)">
+<meta name="MobileOptimized" content="width">
+<meta name="HandheldFriendly" content="true">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="theme-color" content="#ffffff">
+
+    <title>Home | Aerobeam</title>
+    <link rel="stylesheet" media="all" href="/css/aerobeam.css">
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.min.js"
+  integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+  crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
